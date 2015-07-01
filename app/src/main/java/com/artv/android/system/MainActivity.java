49e4f19887ce7dff0cb607d9df48023270be8cdb
ZOG <zdesbulbac9i@gmail.com>
2015-07-01 12:44:28 +0300

    Removed crash btn from MainActivity. Merged with fabric crashlitycs branch.

--- app/src/main/java/com/artv/android/system/MainActivity.java
+++ app/src/main/java/com/artv/android/system/MainActivity.java
@@ -18,13 +18,6 @@ public class MainActivity extends Activity {
         super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Button) findViewById(R.id.btnCrash_AM)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public final void onClick(View v) {
-                throw new RuntimeException("Test crash yo");
-            }
-        });
-
         mFragmentContainer = (FrameLayout) findViewById(R.id.flFragmentContainer_MA);
 
         getFragmentManager().beginTransaction().
