bfbb4732e2bae35030a12213778098b2259feb78
ZOG <zdesbulbac9i@gmail.com>
2015-07-10 16:53:29 +0300

    Activity now fullscreen. Updated media player.

--- app/src/main/java/com/artv/android/core/model/MsgBoardCampaign.java
+++ app/src/main/java/com/artv/android/core/model/MsgBoardCampaign.java
@@ -13,30 +13,30 @@ import java.util.List;
 public final class MsgBoardCampaign {
 
     @Element(name = "MsgBoardID", required = false)
-    private long mMsgBoardID;
+    public long mMsgBoardID;
 
     @Element(name = "CRCVersion", required = false)
-    private int mCRCVersion;
+    public int mCRCVersion;
 
     @Element(name = "StartDate", required = false)
-    private String mStartDate;
+    public String mStartDate;
 
     @Element(name = "EndDate", required = false)
-    private String mEndDate;
+    public String mEndDate;
 
     @Element(name = "PlayDay", required = false)
-    private String mPlayDay;
+    public String mPlayDay;
 
     @Element(name = "TextColor", required = false)
-    private String mTextColor;
+    public String mTextColor;
 
     @Element(name = "RightBkgURL", required = false)
-    private String mRightBkgURL;
+    public String mRightBkgURL;
 
     @Element(name = "BottomBkgURL", required = false)
-    private String mBottomBkgURL;
+    public String mBottomBkgURL;
 
     @ElementList(name = "Messages", required = false)
-    private List<Message> mMessages;
+    public List<Message> mMessages;
 
 }
