15cd5dfe07fa267c20820e2a7a7c205395a59bdd
ZOG <zdesbulbac9i@gmail.com>
2015-08-10 14:55:40 +0300

    Added new methods to DbWorker.

--- app/src/main/java/com/artv/android/database/DbWorker.java
+++ app/src/main/java/com/artv/android/database/DbWorker.java
@@ -2,6 +2,7 @@ package com.artv.android.database;
 
 import com.artv.android.core.model.Asset;
 import com.artv.android.core.model.Campaign;
+import com.artv.android.core.model.CampaignInfo;
 
 import java.util.List;
 
@@ -10,13 +11,16 @@ import java.util.List;
  */
 public interface DbWorker {
 
-    boolean contains(final Campaign _campaign);
-    boolean contains(final Asset _asset);
-    void write(final Campaign _campaign);
     void write(final Asset _asset);
-
-    List<Campaign> getAllCampaigns();
+    boolean contains(final Asset _asset);
     List<Asset> getAllAssets();
 
+    void write(final Campaign _campaign);
+    boolean contains(final Campaign _campaign);
+    List<Campaign> getAllCampaigns();
     List<Asset> getAssets(final Campaign _campaign);
+
+    void write(final CampaignInfo _campaignInfo);
+    boolean contains(final CampaignInfo _campaignInfo);
+    List<CampaignInfo> getAllCampaignInfos();
 }
